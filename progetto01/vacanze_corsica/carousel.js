e955daa4530ad3918dbb23d7d0aaad96d2736114
PAOLO ORTALDA <154626945+POrtalda@users.noreply.github.com>
2024-08-14 08:29:05 +0200

    sistemato il lato pubblico

--- progetto01/vacanze_corsica/carousel.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var myCarousel = document.querySelector('#foto');
-    var carousel = new bootstrap.Carousel(myCarousel, {
-        interval: 5000, // 5000ms = 5 seconds
-        wrap: true
-    });
-});
